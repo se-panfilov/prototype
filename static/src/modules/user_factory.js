2ffa6f7b4797cb37ea96f70134404867f63dda70
S.Panfilov <se-panfilov@ya.ru>
2015-10-20 16:12:21 +0800

    fixes

--- static/src/modules/user_factory.js
+++ static/src/modules/user_factory.js
@@ -14,75 +14,35 @@ angular.module('app.user.factory', [])
             )
         };
 
-        function getAllFromLocalStorage() {
-            var users = [];
-            var usersStr = localStorage.getItem('users');
-
-            users = (usersStr && usersStr.length > 0) ? JSON.parse(usersStr) : users;
-
-            return users;
-        }
-
-        function getFromLocalStorage(id) {
-            var users = [];
-            var usersStr = localStorage.getItem('users');
-            var result;
-
-            users = (usersStr && usersStr.length > 0) ? JSON.parse(usersStr) : users;
-
-            for (var i = 0; i < users.length; i++) {
-                var user = users[i];
-                if (user.id === id) {
-                    result = user;
-                }
-            }
-
-            return result;
-        }
-
         //Because there is no backend, e will use lockal storage
         return {
             getUsersList: function () {
-                return getAllFromLocalStorage();
-
-                //For example
-
-                //return _userApi.user.get(function () {
-                //    //
-                //}, function (response) {
-                //    //show some error
-                //}).$promise;
+                return _userApi.user.get(function () {
+                    //
+                }, function (response) {
+                    //show some error
+                }).$promise;
             },
             getUser: function (id) {
-                return getFromLocalStorage(id);
-
-                //For example
-
-                //return _userApi.user.get({id: id}, function () {
-                //    //
-                //}, function (response) {
-                //    //show some error
-                //}).$promise;
+                return _userApi.user.get({id: id}, function () {
+                    //
+                }, function (response) {
+                    //show some error
+                }).$promise;
             },
             addUser: function () {
-
-                //For example
-
-                //return _userApi.user.add(function () {
-                //    //
-                //}, function (response) {
-                //    //show some error
-                //}).$promise;
+                return _userApi.user.add(function () {
+                    //
+                }, function (response) {
+                    //show some error
+                }).$promise;
             },
             removeUser: function (id) {
-
-                //For example
-
-                //return _userApi.user.remove({id: id}, function () {
-                //    //
-                //}, function (response) {
-                //    //show some error
-                //}).$promise;
+                return _userApi.user.remove({id: id}, function () {
+                    //
+                }, function (response) {
+                    //show some error
+                }).$promise;
             }
         };
 
