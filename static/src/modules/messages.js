f59db86fa9ef3239e714b8fafee274eb3872a695
S.Panfilov <se-panfilov@ya.ru>
2015-10-20 15:18:16 +0800

    added messages module

---
+++ static/src/modules/messages.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('app.messages', ['toaster'])
+
+    .factory('MessagesFactory', function (toaster) {
+
+        var _messageTypes = {
+            success: 'success',
+            error: 'error',
+        };
+
+        var _messagesTitles = {
+            error: 'Error'
+        };
+
+        var exports = {
+            showSuccessMsg: function (message, title) {
+                title = title || null;
+
+                toaster.pop(_messageTypes.success, title, message);
+            },
+            showErrorMsg: function (message, title) {
+                title = title || _messagesTitles.error;
+
+                toaster.pop(_messageTypes.error, title, message);
+            }
+        };
+
+        return exports;
+    })
+;
